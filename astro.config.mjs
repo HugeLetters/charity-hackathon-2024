17063e758dccce8c69e31c4ed51972892ff1c10b
Evgenii Perminov <evperminov@ozon.ru>
2024-09-28 00:32:17 +0200

    add lightningcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
+
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
 	build: {
 		inlineStylesheets: "never",
+	},
+	vite: {
+		css: {
+			transformer: "lightningcss",
+			lightningcss: {
+				targets: browserslistToTargets(browserslist(">= 0.25%")),
+			},
+		},
+		build: {
+			cssMinify: "lightningcss",
+		},
 	},
 	integrations: [purgecss()],
 });
