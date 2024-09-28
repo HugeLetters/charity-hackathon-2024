6d4b73a2b4a509676b7da86dd7ecbf2c46ccd22f
Evgenii Perminov <evperminov@ozon.ru>
2024-09-29 01:31:43 +0200

    horizontal slider helper

---
+++ src/components/shared/slider.ts
@@ -0,0 +1,36 @@
+type SliderOptions = {
+	slider: HTMLElement;
+	prevButton?: HTMLElement;
+	nextButton?: HTMLElement;
+};
+
+export function createHorizontalSlider({
+	slider,
+	prevButton,
+	nextButton,
+}: SliderOptions) {
+	const firstChild = slider.firstElementChild;
+	if (!firstChild) {
+		throw new Error("Trying to create slider with no children", {
+			cause: slider,
+		});
+	}
+
+	const offset = firstChild.clientWidth;
+	prevButton?.addEventListener("click", () => {
+		if (slider.scrollLeft <= 10) {
+			slider.scroll({ left: slider.scrollWidth, behavior: "smooth" });
+			return;
+		}
+		slider.scrollBy({ left: -offset, behavior: "smooth" });
+	});
+
+	nextButton?.addEventListener("click", () => {
+		if (slider.scrollWidth <= slider.scrollLeft + slider.clientWidth) {
+			slider.scroll({ left: 0, behavior: "smooth" });
+			return;
+		}
+
+		slider.scrollBy({ left: offset, behavior: "smooth" });
+	});
+}
